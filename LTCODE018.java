25d6156024d5d2c9d42bb8af13423a7087858070
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-02-18 10:04:18 +0530

    Merge Intervals

---
+++ LTCODE018.java
@@ -0,0 +1,26 @@
+public class LTCODE018 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        
+        merge(nums1, m, nums2, n);
+        
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
