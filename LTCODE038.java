26edc94afa4c2810f01babbed1cdc9d79692a795
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-03-11 10:19:28 +0530

    Path Sum

---
+++ LTCODE038.java
@@ -0,0 +1,24 @@
+ /* Path Sum */
+
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class LTCODE038 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val); 
+    }
+}
