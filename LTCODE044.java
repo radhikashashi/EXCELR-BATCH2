f4bfb7f1722f29a7f55d9cf45282381c1c69bb95
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-03-17 10:00:41 +0530

    Array Partition

---
+++ LTCODE044.java
@@ -0,0 +1,14 @@
+/* Array Partition */
+
+
+import java.util.*;
+class LTCODE044 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
