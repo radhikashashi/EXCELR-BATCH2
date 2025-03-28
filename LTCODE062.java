27f345ec45cfa86b5e8aa17eb55cf61e898c2df2
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-03-28 10:10:46 +0530

    Find the Index of the First Occurrence in a String

---
+++ LTCODE062.java
@@ -0,0 +1,12 @@
+/* Find the Index of the First Occurrence in a String */
+
+class LTCODE062 {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
